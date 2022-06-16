6076b43b4df9e4fa792cd410f2ee45b56b8974c5
nikcoder12 <100820996+nikcoder12@users.noreply.github.com>
2022-06-16 07:19:27 +0530

    Update prob 1.cpp

--- Practical/week5/prob 1.cpp
+++ Practical/week5/prob 1.cpp
@@ -1,38 +1,42 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
+void findmax(vector<char>&v,int &max,char ch)
+{
+   vector<int>count(256,0);
+ 
+   for(int i=0;i<v.size();i++)
+   {
+       int a=v[i];
+      
+       count[a]++;
+       if(max<count[a])
+       {
+           max=count[a];
+           ch=a;
+           
+       }
+   }
+  
+ }
 int main()
 {
-    int n1, n2;
-    cin >> n1;
-    int arr1[n1];
-    for (int i = 0; i < n1; i++)
-        cin >> arr1[i];
-
-    cin >> n2;
-    int arr2[n2];
-    for (int i = 0; i < n2; i++)
-        cin >> arr2[i];
-
-    int ptr1 = 0, ptr2 = 0;
-    int ans_ptr = 0;
-    int n3 = (n1 > n2) ? n1 : n2;
-    int ans[n3];
-    while (ptr1 < n1 and ptr2 < n2)
+    int t;
+    cin>>t;
+    while(t--)
     {
-        if (arr1[ptr1] == arr2[ptr2])
+        int n,max=0;
+        char ch;
+        cin>>n;
+        vector<char>v;
+        for(int i=0;i<n;i++)
         {
-            ans_arr[ans_ptr++] = arr1[ptr1++];
-            ptr2++;
+            char c;
+            cin>>c;
+            v.push_back(c);
         }
-        else if (arr1[ptr1] < arr2[ptr2])
-            ptr1++;
-        else
-            ptr2++;
+          findmax(v,max,ch);
+          if(max<=1)cout<<"No duplicate found!"<<endl;
+          else cout<<ch<<" "<<max<<endl;
     }
-
-    for (int i = 0; i < ans_ptr; i++)
-        cout << ans[i] << " ";
-    cout << endl;
-    return 0;
 }
